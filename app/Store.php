9b4a8eaa2f5eea7b349c60f960e0d66139349beb
unknown <beiker2203@gmail.com>
2019-12-20 00:49:52 -0500

    Realizando las relaciones entre los modelos basicos

---
+++ app/Store.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Store extends Model
+{
+    public function category()
+    {
+        return $this->belongsTo('App\Category');
+    }
+
+    public function seller()
+    {
+        return $this->belongsTo('App\Seller');
+    }
+}
--- app/User.php
+++ app/User.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-    
+    public function seller() {
+        return $this->hasOne('App\Seller');
+    }
+
+    public function client() {
+        return $this->hasOne('App\Client');
+    }
+
+    public function admin() {
+        return $this->hasOne('App\Admin');
+    }
 }
